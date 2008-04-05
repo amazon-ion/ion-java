cfe4a59d0010079eed18cd2be3db14f96509d0ea
Chris Suver <csuver@amazon.com>
2008-04-05 18:22:42 +0000

    cleaned up the warning errors
    [GF:1767698:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- src/com/amazon/ion/util/Equivalence.java
+++ src/com/amazon/ion/util/Equivalence.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
