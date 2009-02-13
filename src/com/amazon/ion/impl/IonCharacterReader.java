c3816cc39214da1f4e0f702fb23f86e9d29bee8f
Todd Jonker <jonker@amazon.com>
2009-02-13 02:25:35 +0000

    Bugfix: Don't normalize LF-CR to LF.
    [GF:2248510:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- src/com/amazon/ion/impl/IonCharacterReader.java
+++ src/com/amazon/ion/impl/IonCharacterReader.java
@@ -12,124 +12,136 @@ import java.util.LinkedList;
 /**
  * Extension of the {@link java.io.PushbackReader} to abstract line/offset counting
  * and push back support.
- * 
+ *
  * Note that this class does not leverage {@link java.io.LineNumberReader} as we
  * need to un-roll the line number on push back and we need to have the logic to
  * deal with newline combinations anyhow.
  */
 public class IonCharacterReader extends PushbackReader {
-    
-    /** The default buffer size */
+
+    /**
+     * The default buffer size
+     *
+     * @see IonImplUtils#MAX_LOOKAHEAD_UTF16
+     */
     public static final int DEFAULT_BUFFER_SIZE = 5;
-    
+
     /**
      * The additional buffer padding--this is to add to the fact that the
      * pushback buffer may have to be larger than user specified characters
      * a CR/LF combo is 2 characters, but logically one newline.
+     *
+     * FIXME does this properly account for surrogates?
+     *
+     * @see IonImplUtils#MAX_LOOKAHEAD_UTF16
      */
     public static final int BUFFER_PADDING = 1;
-    
+
     private int m_consumed;
     private int m_line;
     private int m_column;
     private int m_size;
-    
+
     // our offset stack--really, for efficiency this should
     // be a linked list of primitive ints to avoid boxing costs
     // however, pursue this only if it is really a performance
     // issue.  Also, we are not using the List interface for code
     // clarity as this is used as a deque. (Java 6 has java.util.Deque)
-    
+
     /**
      * This offset stack is for pushing back characters that unroll lines
      * and keeps our offset/line number counts correct.  This is essentially
      * a pushback stack for character offsets.
      */
     private LinkedList<Integer> m_columns;
-    
+
     /**
      * Constructs a character reader with an explicit buffer size.  Note that this
      * is a minimum and the implementation is allowed to make it larger for
      * internal operations.
-     * 
+     *
      * @param in    The underlying reader to wrap.
      * @param size  The size of the push back buffer.
      */
     public IonCharacterReader( final Reader in, final int size ) {
         super( in, size + BUFFER_PADDING );
-        
+
         assert size > 0;
-        
+
         m_consumed = 0;
         m_line = 1;
         m_column = 0;
         m_columns = new LinkedList<Integer>();
         m_size = size + BUFFER_PADDING;
     }
-    
+
     /**
      * Constructs a character reader with the default buffer size.
-     * 
+     *
      * @param in    The underlying reader to wrap.
      */
     public IonCharacterReader( final Reader in ) {
         this( in, DEFAULT_BUFFER_SIZE );
     }
-    
+
     /**
      * Returns the logical number of consumed characters.
      * This does not necessarily equal to the number of
      * actual characters read as newline combinations are
      * treated as one.
-     * 
+     *
      * @return The logical number of consumed characters.
      */
     public final int getConsumedAmount() {
         return m_consumed;
     }
-    
+
     /**
      * Returns the current line number in the stream based on the last call
      * to read().
-     * 
+     *
      * @return  The line number, 1-based.
      */
     public final int getLineNumber() {
         return m_line;
     }
-    
+
     /**
      * Returns the offset within the line based on the last call to read().
-     * 
+     *
      * @return  The offset, 1-based.
      */
     public final int getColumn() {
         return m_column;
     }
-    
+
     /**
-     * Uses the push back implementation but normalizes newlines to "\n". 
+     * Uses the push back implementation but normalizes newlines to "\n".
      */
     @Override
     public int read() throws IOException {
         int nextChar = super.read();
-        
+
         // process the character
         if ( nextChar != -1 ) {
-            if ( nextChar == '\n' || nextChar == '\r') {
-                int aheadChar = super.read();
-                
-                // if the lookahead is not a newline combo, unread it
-                if ( ( nextChar == '\r' && aheadChar != '\n' )
-                        || ( nextChar == '\n' && aheadChar != '\r' ) ) {
-                    // no need to unread it with line/offset update.
-                    unreadImpl( aheadChar, false );
-                }
-                
+            if ( nextChar == '\n' ) {
                 m_line++;
                 pushColumn( m_column );
                 m_column = 0;
-                
+            }
+            else if ( nextChar == '\r') {
+                int aheadChar = super.read();
+
+                // if the lookahead is not a newline combo, unread it
+                if ( aheadChar != '\n' ) {
+                    // no need to unread it with line/offset update.
+                    unreadImpl( aheadChar, false );
+                }
+
+                m_line++;
+                pushColumn( m_column );
+                m_column = 0;
+
                 // normalize
                 nextChar = '\n';
             } else {
@@ -137,7 +149,7 @@ public class IonCharacterReader extends PushbackReader {
             }
             m_consumed++;
         }
-            
+
         return nextChar;
     }
 
@@ -147,16 +159,16 @@ public class IonCharacterReader extends PushbackReader {
         if ( m_columns.size() == m_size ) {
             m_columns.removeFirst();
         }
-        
+
         // box into collection
         m_columns.addLast( offset );
     }
-    
+
     private final int popColumn() throws IOException {
         if ( m_columns.isEmpty() ) {
             throw new IOException( "Cannot unread past buffer" );
         }
-        
+
         // unbox out of collection
         return m_columns.removeLast();
     }
@@ -169,7 +181,7 @@ public class IonCharacterReader extends PushbackReader {
     public int read( char[] cbuf, int off, int len ) throws IOException {
         assert len >= 0;
         assert off >= 0;
-        
+
         int amountRead = 0;
         final int endIndex = off + len;
         for ( int index = off; index < endIndex; index++ ) {
@@ -177,11 +189,11 @@ public class IonCharacterReader extends PushbackReader {
             if ( readChar == -1 ) {
                 break;
             }
-            
+
             cbuf[ index ] = ( char ) readChar;
             amountRead++;
         }
-        
+
         return amountRead == 0 ? -1 : amountRead;
     }
 
@@ -192,13 +204,13 @@ public class IonCharacterReader extends PushbackReader {
     @Override
     public long skip( final long n ) throws IOException {
         assert n > 0;
-        
+
         long charsLeft = n;
         // note the read side effect
         while ( charsLeft > 0 && read() != -1 ) {
             charsLeft--;
         }
-        return n - charsLeft; 
+        return n - charsLeft;
     }
 
     /**
@@ -208,7 +220,7 @@ public class IonCharacterReader extends PushbackReader {
     public void unread( char[] cbuf, int off, int len ) throws IOException {
         assert len >= 0;
         assert off >= 0;
-        
+
         final int endIndex = off + len;
         for ( int index = endIndex - 1; index >= off; index-- ) {
             unread( cbuf[ index ] );
@@ -233,13 +245,13 @@ public class IonCharacterReader extends PushbackReader {
         if ( c == '\r' ) {
             throw new IOException( "Cannot unread a carriage return" );
         }
-        
+
         unreadImpl( c, true );
     }
 
     /**
      * Performs ths actual unread operation.
-     * 
+     *
      * @param c the character to unread.
      * @param updateCounts Whether or not we actually update the line number.
      */
@@ -257,5 +269,5 @@ public class IonCharacterReader extends PushbackReader {
             super.unread( c );
         }
     }
-    
+
 }
