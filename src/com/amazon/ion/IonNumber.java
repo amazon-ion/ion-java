d4c1c346993c0294b3c8295555f136d015f70e44
Chris Suver <csuver@amazon.com>
2009-02-27 06:30:09 +0000

    missed file from negative zero checkin
    [GF:2272663:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

---
+++ src/com/amazon/ion/IonNumber.java
@@ -0,0 +1,44 @@
+package com.amazon.ion;
+
+/**
+ * The IonNumber interface is a fore runner of a common base for the
+ * ion numeric value types.  Currently only IonDecimal extends this
+ * interface. In due course IonFloat and IonInt will be added to the
+ * the family.
+ * 
+ * @author csuver
+ *
+ */
+public interface IonNumber 
+	extends IonValue 
+{
+	public enum Classification {
+		NORMAL,
+		NEGATIVE_ZERO,
+		NAN,
+		NEGATIVE_INFINITY,
+		POSITIVE_INFINITY
+	}
+	
+    /**
+     * Sets the value of this element to be <code>-0d0</code>.  Note that
+     * the <code>java.math.BigDecimal</code> class does not support the 
+     * IEEE negative zero value.  This method allows
+     * you to force this element to have special values such as negative zero.
+     *
+     * @param valueClassification classification to the value
+     */
+    public void setValueSpecial(IonNumber.Classification valueClassification);
+
+
+    /**
+     * This allows you to detect the various special values floating
+     * point and decimal numbers might contain.  IonDecimal
+     * values may only be NORMAL or NEGATIVE_ZERO.  IonFloat values
+     * may have any Classification.
+     * 
+     * @return Classification of the value this element 
+     */
+    public Classification classification();
+
+}
