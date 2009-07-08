8ef8563dcca53252eb3ae2e588b99a318f167a1b
Todd Jonker <jonker@amazon.com>
2009-07-08 23:48:12 +0000

    Doc correction.
    [GF:2520409:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- src/com/amazon/ion/UnexpectedEofException.java
+++ src/com/amazon/ion/UnexpectedEofException.java
@@ -1,13 +1,11 @@
-/*
- * Copyright (c) 2007 Amazon.com, Inc.  All rights reserved.
- */
+// Copyright (c) 2007-2009 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.ion;
 
 
 /**
- * An error caused by invoking an inappropriate method on an Ion
- * <code>null</code> value.
+ * An error caused by processing an Ion input stream that ends in the middle of
+ * a value.
  */
 public class UnexpectedEofException
     extends IonException
