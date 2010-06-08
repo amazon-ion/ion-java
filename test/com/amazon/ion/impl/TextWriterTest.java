16743957f2341ad7e287d0279dcb430b5e241252
Todd Jonker <jonker@amazon.com>
2010-06-08 01:33:48 +0000

    Added test case asserting that the TextWriter doesn't write local symbol tables. cr:almann,pmarie
    [GF:3181893:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- test/com/amazon/ion/impl/TextWriterTest.java
+++ test/com/amazon/ion/impl/TextWriterTest.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2009-2010 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.ion.impl;
 
@@ -33,5 +33,28 @@ public class TextWriterTest
 //        String ionText = new String(utf8Bytes, "UTF-8"); // for debugging
 
         return utf8Bytes;
+    }
+
+    protected String outputString()
+        throws Exception
+    {
+        byte[] utf8Bytes = outputByteArray();
+        return new String(utf8Bytes, "UTF-8");
+    }
+
+    public void testNotWritingSymtab()
+        throws Exception
+    {
+        IonWriter writer = makeWriter();
+        writer.writeSymbol("holla");
+        String ionText = outputString();
+
+        if (! ionText.startsWith(UnifiedSymbolTable.ION_1_0)) {
+            fail("TextWriter didn't write IVM: " + ionText);
+        }
+
+        if (ionText.contains(UnifiedSymbolTable.ION_SYMBOL_TABLE)) {
+            fail("TextWriter shouldn't write symtab: " + ionText);
+        }
     }
 }
