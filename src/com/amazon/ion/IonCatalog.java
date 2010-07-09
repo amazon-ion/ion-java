c6be6afe6c7cb81fa552f98f7946edeff1fd700d
Todd Jonker <jonker@amazon.com>
2010-07-09 19:10:36 +0000

    Expanded JavaDocs of IonCatalog based on user questions.
    [GF:3257322:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- src/com/amazon/ion/IonCatalog.java
+++ src/com/amazon/ion/IonCatalog.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2007-2009 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2007-2010 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.ion;
 
@@ -6,18 +6,45 @@ import com.amazon.ion.system.SimpleCatalog;
 import com.amazon.ion.system.SystemFactory;
 
 
-
 /**
  * Collects shared symbol tables for use by an {@link IonSystem}.
  * <p>
  * It is expected that many applications will implement this interface to
  * customize behavior beyond that provided by the default {@link SimpleCatalog}.
- * A typical implementation would retrieval symbol tables from some external
+ * A typical implementation would retrieve symbol tables from some external
  * source.
  * <p>
  * To utilize a custom catalog, it must be passed to
  * {@link SystemFactory#newSystem(IonCatalog)} when a system is created, or to
  * selected methods of the {@link IonSystem} for localized use.
+ *
+ * <h2>Notes for Implementors</h2>
+ * This interface defines two methods with subtly different semantics.
+ * The first variant takes only a symbol table name, and returns the highest
+ * version possible. The second takes a version number and attempts to match it
+ * exactly, and if that's not possible falls back to the the highest version
+ * possible
+ * <p>
+ * When encoding Ion binary data, its always best to use an exact match to the
+ * requested version whenever possible.  Earlier versions are very likely to be
+ * missing symbols that are needed by the data.  Later versions of the table
+ * could have the same problem, but that's less likely under best practices.
+ * <p>
+ * While "get latest version" is generally okay for encoding, it's not
+ * universally acceptable: one can imagine a client/server protocol where the
+ * client declares what symtab/versions it can handle, and the server needs to
+ * meet those requirements.
+ * <p>
+ * Binary <em>decoding</em> prefers an exact match, and in a couple edge cases,
+ * requires it. Therefore a single "get latest version" method is insufficient.
+ * See <a href="https://w?Ion/Symbols">https://w?Ion/Symbols</a> for more
+ * details on this topic.
+ * <p>
+ * It's expected that many if not most applications will implement a dynamic
+ * catalog that can fetch symtabs from some source.  In such cases the catalog
+ * should make its best effort to find an exact match, and if that's not
+ * possible fall back to the highest version it can acquire.
+ *
  */
 public interface IonCatalog
 {
@@ -33,11 +60,16 @@ public interface IonCatalog
 
 
     /**
-     * Gets a desired symbol table from this catalog.
+     * Gets a desired symbol table from this catalog, using an exact match if
+     * possible.
+     * <p>
+     * Implentations must make a best effort to find an exact match.
+     * If an exact match cannot be found, then this method must make a best
+     * effort to find the highest available version.
      *
      * @return the shared symbol table with the given name and version, when an
-     * exact match is possible. Otherwise, returns the highest known version of
-     * the requested table.  If the catalog has no table with the name, then
+     * exact match is possible. Otherwise, returns the highest possible version
+     * of the requested table.  If no table with the name can be found, then
      * this method returns {@code null}.
      */
     public SymbolTable getTable(String name, int version);
