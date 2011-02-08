5694194b5aed0005a0cd122de32397b27515ce80
Todd Jonker <jonker@amazon.com>
2011-02-08 22:37:31 +0000

    Removed dead code.
    [GF:3802122:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- src/com/amazon/ion/util/IonTextUtils.java
+++ src/com/amazon/ion/util/IonTextUtils.java
@@ -125,37 +125,6 @@ public class IonTextUtils
         return false;
     }
 
-    public static int digitValue(int codePoint, int radix)
-    {
-        assert(isDigit(codePoint, radix) == true);
-
-        switch (codePoint) {
-        case '0':       return 0;
-        case '1':       return 1;
-        case '2':       return 2;
-        case '3':       return 3;
-        case '4':       return 4;
-        case '5':       return 5;
-        case '6':       return 6;
-        case '7':       return 7;
-        case '8':       return 8;
-        case '9':       return 9;
-        case 'A':
-        case 'a':       return 10;
-        case 'B':
-        case 'b':       return 11;
-        case 'C':
-        case 'c':       return 12;
-        case 'D':
-        case 'd':       return 13;
-        case 'E':
-        case 'e':       return 14;
-        case 'F':
-        case 'f':       return 15;
-        }
-        return -1;
-    }
-
     private static boolean is8bitValue(int v) {
         return (v & ~0xff) == 0;
     }
