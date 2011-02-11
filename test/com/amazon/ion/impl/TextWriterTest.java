d6db0158befc42834deb43174f27758d7b862a3e
Todd Jonker <jonker@amazon.com>
2011-02-11 02:02:01 +0000

    Extracted superclass OutputStreamWriterTestCase from TextWriterTest so we can share the flush/close validation.
    [GF:3811134:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- test/com/amazon/ion/impl/TextWriterTest.java
+++ test/com/amazon/ion/impl/TextWriterTest.java
@@ -1,12 +1,9 @@
-// Copyright (c) 2009-2010 Amazon.com, Inc.  All rights reserved.
+// Copyright (c) 2009-2011 Amazon.com, Inc.  All rights reserved.
 
 package com.amazon.ion.impl;
 
 import com.amazon.ion.IonWriter;
 import com.amazon.ion.SymbolTable;
-import java.io.ByteArrayOutputStream;
-import java.io.FilterOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import org.junit.Test;
 
@@ -14,63 +11,14 @@ import org.junit.Test;
  *
  */
 public class TextWriterTest
-    extends IonWriterTestCase
+    extends OutputStreamWriterTestCase
 {
-    private ByteArrayOutputStream myOutputStream;
-    private OutputStreamWrapper myOutputStreamWrapper;
-    private IonWriter myWriter;
-
-
-    private class OutputStreamWrapper extends FilterOutputStream
-    {
-        boolean flushed = false;
-        boolean closed  = false;
-
-        public OutputStreamWrapper(OutputStream out)
-        {
-            super(out);
-        }
-
-        @Override
-        public void flush() throws IOException
-        {
-            flushed = true;
-            super.flush();
-        }
-
-        @Override
-        public void close() throws IOException
-        {
-            closed = true;
-            super.close();
-        }
-    }
-
-
 
     @Override
-    protected IonWriter makeWriter(SymbolTable... imports)
+    protected IonWriter makeWriter(OutputStream out, SymbolTable... imports)
         throws Exception
     {
-        myOutputStream = new ByteArrayOutputStream();
-        myOutputStreamWrapper = new OutputStreamWrapper(myOutputStream);
-        myWriter = system().newTextWriter(myOutputStreamWrapper, imports);
-        return myWriter;
-    }
-
-    @Override
-    protected byte[] outputByteArray()
-        throws Exception
-    {
-        myWriter.close();
-        assertTrue("output stream not flushed", myOutputStreamWrapper.flushed);
-        assertTrue("output stream not closed",  myOutputStreamWrapper.closed);
-
-        byte[] utf8Bytes = myOutputStream.toByteArray();
-
-//        String ionText = new String(utf8Bytes, "UTF-8"); // for debugging
-
-        return utf8Bytes;
+        return system().newTextWriter(out, imports);
     }
 
     protected String outputString()
