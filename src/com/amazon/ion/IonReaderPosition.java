06b7717709820ee13a75211aee7265cce87ed293
Chris Suver <csuver@amazon.com>
2011-07-07 20:11:14 +0000

    missed files from previous checkin
    [GF:4255379:BrazilPerforce://brazil/src/shared/platform/IonJava/development]

---
+++ src/com/amazon/ion/IonReaderPosition.java
@@ -0,0 +1,12 @@
+// Copyright (c) 2011 Amazon.com, Inc.  All rights reserved.
+
+package com.amazon.ion;
+
+/**
+ *  This interface defines the objects which
+ *  can hold a readers current value position.
+ */
+public interface IonReaderPosition
+{
+
+}
