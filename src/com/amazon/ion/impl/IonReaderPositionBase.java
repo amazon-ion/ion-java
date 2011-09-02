37742fa9032454cff7aa8baa3b228be389e7575d
Todd Jonker <jonker@amazon.com>
2011-09-02 18:49:56 +0000

    Fixed newlines.
    [GF:4476323:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- src/com/amazon/ion/impl/IonReaderPositionBase.java
+++ src/com/amazon/ion/impl/IonReaderPositionBase.java
@@ -1,3 +1,17 @@
 // Copyright (c) 2011 Amazon.com, Inc.  All rights reserved.
 package com.amazon.ion.impl;
-/** * Provides a simple implementation of {@link IonReaderPosition} * that delegates facet interpolation as a cast. */public abstract class IonReaderPositionBase implements IonReaderPosition{    public final <T> T asFacet(final Class<T> type)    {        if (!type.isInstance(this))        {            return null;        }        return type.cast(this);    }}
+/**
+ * Provides a simple implementation of {@link IonReaderPosition}
+ * that delegates facet interpolation as a cast.
+ */
+public abstract class IonReaderPositionBase implements IonReaderPosition
+{
+    public final <T> T asFacet(final Class<T> type)
+    {
+        if (!type.isInstance(this))
+        {
+            return null;
+        }
+        return type.cast(this);
+    }
+}
