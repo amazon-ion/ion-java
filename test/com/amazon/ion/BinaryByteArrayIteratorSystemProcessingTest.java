036b2ce71b21e21cccb8de5f9bcc30ed4663d856
Todd Jonker <jonker@amazon.com>
2012-09-26 17:42:45 +0000

    Test case refactoring: split IteratorSystemProcessingTest into IteratorSystemProcessingTestCase and TextIteratorSystemProcessingTest.
    [GF:6245813:BrazilPerforce://brazil/src/shared/platform/IonJava/mainline]

--- test/com/amazon/ion/BinaryByteArrayIteratorSystemProcessingTest.java
+++ test/com/amazon/ion/BinaryByteArrayIteratorSystemProcessingTest.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 
 
 public class BinaryByteArrayIteratorSystemProcessingTest
-    extends IteratorSystemProcessingTest
+    extends IteratorSystemProcessingTestCase
 {
     private byte[] myBytes;
 
--- test/com/amazon/ion/DatagramIteratorSystemProcessingTest.java
+++ test/com/amazon/ion/DatagramIteratorSystemProcessingTest.java
@@ -10,7 +10,7 @@ import java.util.Iterator;
  *
  */
 public class DatagramIteratorSystemProcessingTest
-    extends IteratorSystemProcessingTest
+    extends IteratorSystemProcessingTestCase
 {
     private String myText;
 
