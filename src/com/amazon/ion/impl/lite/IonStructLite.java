2a892ec0e6e22c2e9931348730a220a5f5cc9606
Tim Griffith <tpgriff@amazon.com>
2016-02-15 07:47:30 -0800

    [IONJAVA-482] Moving 'mapify' behavior of struct from 'eager' to 'lazy' to avoid unnecessary costs in deserialization on structs that are never accessed in a get() style manner my the consuming code
    cr https://cr.amazon.com/r/4853992/

--- src/com/amazon/ion/impl/lite/IonStructLite.java
+++ src/com/amazon/ion/impl/lite/IonStructLite.java
@@ -68,14 +68,6 @@ final class IonStructLite
         return clone(ContainerlessContext.wrap(getSystem()));
     }
 
-    @Override
-    protected void transitionToLargeSize(int size)
-    {
-        if (_field_map != null) return;
-
-        build_field_map();
-        return;
-    }
     protected void build_field_map()
     {
         int size = (_children == null) ? 0 : _children.length;
@@ -406,10 +398,18 @@ final class IonStructLite
     {
         validateFieldName(fieldName);
 
-        if (isNullValue()) {
+        if (isNullValue() || _children == null) {
             // nothing to see here, move along
         }
-        else if (_field_map != null) {
+        // if the number of values is greater than the Mapify threshold - get() will be
+        // conducted against the _field_map rather than by iterating the children
+        else if (_children.length > IonContainerLite.STRUCT_CHILDREN_MAPIFY_THRESHOLD) {
+            // field map is *lazy initialized* to avoid building maps during deserialization
+            // which are then never accessed in the consuming logic in a 'map' style fashion
+            // i.e. are never visited or are iterated over as a container
+            if (_field_map == null) {
+                build_field_map();
+            }
             Integer idx = _field_map.get(fieldName);
             if (idx != null) {
                 return idx.intValue();
