1fa65a0cea420704d2d326df7b70eecdebb55f84
Almann Goo <almann@amazon.com>
2016-04-18 16:51:31 -0700

    Makes the style consistent with other files.

--- src/com/amazon/ion/util/JarInfo.java
+++ src/com/amazon/ion/util/JarInfo.java
@@ -21,75 +21,96 @@ import java.io.InputStream;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
+
 /**
  * Provides information about this release of the ion-java library.
  */
-public final class JarInfo {
+public final class JarInfo
+{
 
-  private static final String MANIFEST_FILE = "/META-INF/MANIFEST.MF";
+    private static final String MANIFEST_FILE = "/META-INF/MANIFEST.MF";
 
-  private String ourProjectVersion;
-  private Timestamp ourBuildTime;
+    private String ourProjectVersion;
 
-  /**
-   * Constructs a new instance that can provide build information about this library.
-   *
-   * @throws IonException if there's a problem loading the build info.
-   */
-  public JarInfo() throws IonException {
-    loadBuildProperties(getClass().getResourceAsStream(MANIFEST_FILE));
-  }
+    private Timestamp ourBuildTime;
 
-  JarInfo(InputStream in) {
-    loadBuildProperties(in);
-  }
-
-  /**
-   * Gets the ion-java project version of this build.
-   *
-   * @return null if the package version is unknown.
-   */
-  public String getProjectVersion() {
-    return ourProjectVersion;
-  }
-
-  /**
-   * Gets the time at which this package was built.
-   *
-   * @return null if the build time is unknown.
-   */
-  public Timestamp getBuildTime() {
-    return ourBuildTime;
-  }
-
-  // TODO writeTo(IonWriter)
-
-  // ========================================================================
-
-  private void loadBuildProperties(InputStream in) throws IonException {
-    try {
-      Manifest manifest = new Manifest();
-      if (in != null) {
-        try {
-          manifest.read(in);
-        } finally {
-          in.close();
-        }
-      }
-      Attributes mainAttributes = manifest.getMainAttributes();
-
-      ourProjectVersion = mainAttributes.getValue("Project-Version");
-
-      String time = mainAttributes.getValue("Build-Time");
-      if (time != null) {
-        try {
-          ourBuildTime = Timestamp.valueOf(time);
-        } catch (IllegalArgumentException e) {
-          // Badly formatted timestamp. Ignore it.
-        }
-      }
-    } catch (IOException e) {
-      throw new IonException("Unable to load manifest.", e);
+    /**
+     * Constructs a new instance that can provide build information about this
+     * library.
+     *
+     * @throws IonException
+     *         if there's a problem loading the build info.
+     */
+    public JarInfo() throws IonException
+    {
+        loadBuildProperties(getClass().getResourceAsStream(MANIFEST_FILE));
     }
-  }
+
+    JarInfo(InputStream in)
+    {
+        loadBuildProperties(in);
+    }
+
+    /**
+     * Gets the ion-java project version of this build.
+     *
+     * @return null if the package version is unknown.
+     */
+    public String getProjectVersion()
+    {
+        return ourProjectVersion;
+    }
+
+    /**
+     * Gets the time at which this package was built.
+     *
+     * @return null if the build time is unknown.
+     */
+    public Timestamp getBuildTime()
+    {
+        return ourBuildTime;
+    }
+
+    // TODO writeTo(IonWriter)
+
+    // ========================================================================
+
+    private void loadBuildProperties(InputStream in) throws IonException
+    {
+        try
+        {
+            Manifest manifest = new Manifest();
+            if (in != null)
+            {
+                try
+                {
+                    manifest.read(in);
+                }
+                finally
+                {
+                    in.close();
+                }
+            }
+            Attributes mainAttributes = manifest.getMainAttributes();
+
+            ourProjectVersion = mainAttributes.getValue("Project-Version");
+
+            String time = mainAttributes.getValue("Build-Time");
+            if (time != null)
+            {
+                try
+                {
+                    ourBuildTime = Timestamp.valueOf(time);
+                }
+                catch (IllegalArgumentException e)
+                {
+                    // Badly formatted timestamp. Ignore it.
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            throw new IonException("Unable to load manifest.", e);
+        }
+    }
 }
