0cc9cc73d959d1c2d5ec2cd176d382038de378b3
Tyler Gregg <greggt@amazon.com>
2016-04-21 16:13:58 -0700

    Retrieving compiled-in jar info now handles encountering multiple manifests on the classpath.

--- test/software/amazon/ion/util/JarInfoTest.java
+++ test/software/amazon/ion/util/JarInfoTest.java
@@ -17,25 +17,92 @@ package software.amazon.ion.util;
 import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.jar.Manifest;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import software.amazon.ion.IonException;
 import software.amazon.ion.Timestamp;
-import software.amazon.ion.util.JarInfo;
 
 public class JarInfoTest
 {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private static String getAttributes(String manifestVersion)
+    {
+        return "Manifest-Version: " + manifestVersion + "\n";
+    }
+
+    private static String getIonJavaAttributes(String buildTime, String version)
+    {
+        return getAttributes("1.0")
+            + "Ion-Java-Build-Time: " + buildTime + "\n"
+            + "Ion-Java-Project-Version: " + version + "\n";
+    }
+
+    private static Manifest getManifest(String attributes) throws IOException
+    {
+        return new Manifest(new ByteArrayInputStream(attributes.getBytes("UTF-8")));
+    }
+
     @Test
-    public void testConstruction() throws Exception
+    public void testSingleManifest() throws Exception
     {
         String expectedBuildTime = "1984T";
         String expectedVersion = "42.0";
 
-        String manifest = "Manifest-Version: 1.0\n"
-                        + "Build-Time: " + expectedBuildTime + "\n"
-                        + "Project-Version: " + expectedVersion + "\n";
-
-        JarInfo info = new JarInfo(new ByteArrayInputStream(manifest.getBytes("UTF-8")));
+        Manifest manifest = getManifest(getIonJavaAttributes(expectedBuildTime, expectedVersion));
+        JarInfo info = new JarInfo(Collections.singletonList(manifest));
 
         assertEquals(expectedVersion, info.getProjectVersion());
         assertEquals(Timestamp.valueOf(expectedBuildTime), info.getBuildTime());
     }
+
+    @Test
+    public void testMultipleManifests() throws Exception
+    {
+        String expectedBuildTime = "1984T";
+        String expectedVersion = "42.0";
+
+        List<Manifest> manifests = new ArrayList<Manifest>();
+        manifests.add(getManifest(getAttributes("1.0")));
+        manifests.add(getManifest(getIonJavaAttributes(expectedBuildTime, expectedVersion)));
+        manifests.add(getManifest(getAttributes("2.0")));
+
+        JarInfo info = new JarInfo(manifests);
+
+        assertEquals(expectedVersion, info.getProjectVersion());
+        assertEquals(Timestamp.valueOf(expectedBuildTime), info.getBuildTime());
+    }
+
+    @Test
+    public void testNoMatchingManifests() throws Exception
+    {
+        List<Manifest> manifests = new ArrayList<Manifest>();
+        manifests.add(getManifest(getAttributes("1.0")));
+        manifests.add(getManifest(getAttributes("2.0")));
+        manifests.add(getManifest(getAttributes("3.0")));
+
+        thrown.expect(IonException.class);
+        new JarInfo(manifests);
+    }
+
+    @Test
+    public void testConflictingManifests() throws Exception
+    {
+        List<Manifest> manifests = new ArrayList<Manifest>();
+        manifests.add(getManifest(getIonJavaAttributes("1984T", "42.0")));
+        manifests.add(getManifest(getAttributes("1.0")));
+        manifests.add(getManifest(getIonJavaAttributes("1985T", "43.0")));
+
+        thrown.expect(IonException.class);
+        new JarInfo(manifests);
+    }
+
 }
