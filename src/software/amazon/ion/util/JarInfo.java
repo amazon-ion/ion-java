d701f5c2c4df3a6c671bb81f08e71624149235e9
Tyler Gregg <tgregg@users.noreply.github.com>
2016-04-21 17:53:37 -0700

    Retrieving compiled-in jar info now handles encountering multiple manifests on the classpath. (#73)

--- src/software/amazon/ion/util/JarInfo.java
+++ src/software/amazon/ion/util/JarInfo.java
@@ -15,7 +15,10 @@
 package software.amazon.ion.util;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 import software.amazon.ion.IonException;
@@ -28,7 +31,9 @@ import software.amazon.ion.Timestamp;
 public final class JarInfo
 {
 
-    private static final String MANIFEST_FILE = "/META-INF/MANIFEST.MF";
+    private static final String MANIFEST_FILE = "META-INF/MANIFEST.MF";
+    private static final String BUILD_TIME_ATTRIBUTE = "Ion-Java-Build-Time";
+    private static final String PROJECT_VERSION_ATTRIBUTE = "Ion-Java-Project-Version";
 
     private String ourProjectVersion;
 
@@ -43,12 +48,33 @@ public final class JarInfo
      */
     public JarInfo() throws IonException
     {
-        loadBuildProperties(getClass().getResourceAsStream(MANIFEST_FILE));
+        Enumeration<URL> manifestUrls;
+        try
+        {
+            manifestUrls = getClass().getClassLoader().getResources(MANIFEST_FILE);
+        }
+        catch (IOException e)
+        {
+            throw new IonException("Unable to load manifests.", e);
+        }
+        List<Manifest> manifests = new ArrayList<Manifest>();
+        while (manifestUrls.hasMoreElements())
+        {
+            try
+            {
+                manifests.add(new Manifest(manifestUrls.nextElement().openStream()));
+            }
+            catch (IOException e)
+            {
+                continue; // try the next manifest
+            }
+        }
+        loadBuildProperties(manifests);
     }
 
-    JarInfo(InputStream in)
+    JarInfo(List<Manifest> manifests)
     {
-        loadBuildProperties(in);
+        loadBuildProperties(manifests);
     }
 
     /**
@@ -75,42 +101,49 @@ public final class JarInfo
 
     // ========================================================================
 
-    private void loadBuildProperties(InputStream in) throws IonException
+    private void loadBuildProperties(List<Manifest> manifests) throws IonException
     {
+        boolean propertiesLoaded = false;
+        for(Manifest manifest : manifests)
+        {
+            boolean success = tryLoadBuildProperties(manifest);
+            if(success && propertiesLoaded)
+            {
+                // In the event of conflicting manifests, fail instead of risking returning incorrect version info.
+                throw new IonException("Found multiple manifests with ion-java version info on the classpath.");
+            }
+            propertiesLoaded |= success;
+        }
+        if (!propertiesLoaded)
+        {
+            throw new IonException("Unable to locate manifest with ion-java version info on the classpath.");
+        }
+    }
+
+    /*
+     * Returns true if the properties were loaded, otherwise false.
+     */
+    private boolean tryLoadBuildProperties(Manifest manifest)
+    {
+        Attributes mainAttributes = manifest.getMainAttributes();
+        String projectVersion = mainAttributes.getValue(PROJECT_VERSION_ATTRIBUTE);
+        String time = mainAttributes.getValue(BUILD_TIME_ATTRIBUTE);
+
+        if (projectVersion == null || time == null)
+        {
+            return false;
+        }
+
+        ourProjectVersion = projectVersion;
+
         try
         {
-            Manifest manifest = new Manifest();
-            if (in != null)
-            {
-                try
-                {
-                    manifest.read(in);
-                }
-                finally
-                {
-                    in.close();
-                }
-            }
-            Attributes mainAttributes = manifest.getMainAttributes();
-
-            ourProjectVersion = mainAttributes.getValue("Project-Version");
-
-            String time = mainAttributes.getValue("Build-Time");
-            if (time != null)
-            {
-                try
-                {
-                    ourBuildTime = Timestamp.valueOf(time);
-                }
-                catch (IllegalArgumentException e)
-                {
-                    // Badly formatted timestamp. Ignore it.
-                }
-            }
+            ourBuildTime = Timestamp.valueOf(time);
         }
-        catch (IOException e)
+        catch (IllegalArgumentException e)
         {
-            throw new IonException("Unable to load manifest.", e);
+            // Badly formatted timestamp. Ignore it.
         }
+        return true;
     }
 }
