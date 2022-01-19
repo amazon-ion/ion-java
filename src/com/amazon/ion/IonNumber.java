025fcf81f1bb1e60d22a3c8ae81d978efe60497d
Matthew Pope <popematt@amazon.com>
2022-01-19 14:57:58 -0800

    Adds members to the IonNumber interface and makes IonDecimal, IonFloat, and IonInt implement IonNumber

--- src/com/amazon/ion/IonNumber.java
+++ src/com/amazon/ion/IonNumber.java
@@ -15,16 +15,45 @@
 
 package com.amazon.ion;
 
+import java.math.BigDecimal;
+
 /**
- * The IonNumber interface is a fore runner of a common base for the
- * ion numeric value types.  Currently only IonDecimal extends this
- * interface. In due course IonFloat and IonInt will be added to the
- * the family.
+ * Common functionality of Ion {@code int}, {@code decimal}, and {@code float}
+ * types.
  * <p>
  * <b>WARNING:</b> This interface should not be implemented or extended by
  * code outside of this library.
  */
-public interface IonNumber // TODO amzn/ion-java/issues/53 Complete this interface
+public interface IonNumber
     extends IonValue
 {
+    /**
+     * Gets the value of this Ion number as a {@link BigDecimal}.
+     *
+     * <p>This method will throw an exception for non-null, non-numeric values.
+     * It's recommended to call {@link #isNumericValue()} before calling
+     * this method.</p>
+     *
+     * <p>Negative zero is supported by {@link IonDecimal} and {@link IonFloat},
+     * but not by {@link BigDecimal}. If you need to distinguish positive and
+     * negative zero, you should call {@link IonDecimal#decimalValue()} or
+     * {@link IonFloat#doubleValue()} after casting to the appropriate type.</p>
+     *
+     * @return the {@link BigDecimal} value, or {@code null} if
+     * {@code this.isNullValue()}.
+     *
+     * @throws NumberFormatException if this value is {@code nan},
+     * {@code +inf}, or {@code -inf}, because {@link BigDecimal} cannot
+     * represent those values.
+     */
+    public BigDecimal bigDecimalValue();
+
+    /**
+     * Determines whether this value is numeric. Returns true if this value
+     * is none of {@code null}, {@code nan}, {@code +inf}, and {@code -inf},
+     * and false if it is any of them.
+     *
+     * @return a checked condition whether this value is numeric.
+     */
+    public boolean isNumericValue();
 }
