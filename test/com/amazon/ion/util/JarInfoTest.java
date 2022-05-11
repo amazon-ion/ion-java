ee2a306d3ecb509973b708afcbf0ddab9dd31887
jobarr-amzn <70981087+jobarr-amzn@users.noreply.github.com>
2022-05-11 12:57:37 -0700

    Jar info changes (#433)
    * adds changes for JarInfo file
    
    * adds build.properties
    * modfies JarInfo build logic
    * removes manifest file logic
    * modifies unit test for JarInfo
    
    * Use filtering to get properties interpolation
    
    Also clean up pom.xml
    
    * Look for the correct .properties file, add tests
    
    Co-authored-by: Khushboo Desai <desaikd@amazon.com>

--- test/com/amazon/ion/util/JarInfoTest.java
+++ test/com/amazon/ion/util/JarInfoTest.java
@@ -15,95 +15,31 @@
 
 package com.amazon.ion.util;
 
-import com.amazon.ion.IonException;
 import com.amazon.ion.Timestamp;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.jar.Manifest;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.matchesPattern;
 
-import static org.junit.Assert.*;
-
-public class JarInfoTest
-{
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    private static String getAttributes(String manifestVersion)
-    {
-        return "Manifest-Version: " + manifestVersion + "\n";
-    }
-
-    private static String getIonJavaAttributes(String buildTime, String version)
-    {
-        return getAttributes("1.0")
-                + "Ion-Java-Build-Time: " + buildTime + "\n"
-                + "Ion-Java-Project-Version: " + version + "\n";
-    }
-
-    private static Manifest getManifest(String attributes) throws IOException
-    {
-        return new Manifest(new ByteArrayInputStream(attributes.getBytes("UTF-8")));
+public class JarInfoTest {
+    @Test
+    public void getBuildTime() {
+        JarInfo info = new JarInfo();
+        Timestamp timestamp = info.getBuildTime();
+        // This test was written at the timestamp below, the loaded time should always be greater
+        Timestamp original = Timestamp.valueOf("2022-05-10T22:56:48Z");
+        assertThat(timestamp, greaterThan(original));
     }
 
     @Test
-    public void testSingleManifest() throws Exception
-    {
-        String expectedBuildTime = "1984T";
-        String expectedVersion = "42.0";
-
-        Manifest manifest = getManifest(getIonJavaAttributes(expectedBuildTime, expectedVersion));
-        JarInfo info = new JarInfo(Collections.singletonList(manifest));
-
-        assertEquals(expectedVersion, info.getProjectVersion());
-        assertEquals(Timestamp.valueOf(expectedBuildTime), info.getBuildTime());
-    }
-
-    @Test
-    public void testMultipleManifests() throws Exception
-    {
-        String expectedBuildTime = "1984T";
-        String expectedVersion = "42.0";
-
-        List<Manifest> manifests = new ArrayList<Manifest>();
-        manifests.add(getManifest(getAttributes("1.0")));
-        manifests.add(getManifest(getIonJavaAttributes(expectedBuildTime, expectedVersion)));
-        manifests.add(getManifest(getAttributes("2.0")));
-
-        JarInfo info = new JarInfo(manifests);
-
-        assertEquals(expectedVersion, info.getProjectVersion());
-        assertEquals(Timestamp.valueOf(expectedBuildTime), info.getBuildTime());
-    }
-
-    @Test
-    public void testNoMatchingManifests() throws Exception
-    {
-        List<Manifest> manifests = new ArrayList<Manifest>();
-        manifests.add(getManifest(getAttributes("1.0")));
-        manifests.add(getManifest(getAttributes("2.0")));
-        manifests.add(getManifest(getAttributes("3.0")));
-
-        thrown.expect(IonException.class);
-        new JarInfo(manifests);
-    }
-
-    @Test
-    public void testConflictingManifests() throws Exception
-    {
-        List<Manifest> manifests = new ArrayList<Manifest>();
-        manifests.add(getManifest(getIonJavaAttributes("1984T", "42.0")));
-        manifests.add(getManifest(getAttributes("1.0")));
-        manifests.add(getManifest(getIonJavaAttributes("1985T", "43.0")));
-
-        thrown.expect(IonException.class);
-        new JarInfo(manifests);
+    public void getProjectVersion() {
+        JarInfo info = new JarInfo();
+        String projectVersion = info.getProjectVersion();
+        // Semantic version MAJOR.MINOR.PATCH with optional -SNAPSHOT suffix
+        // Should always match our project version
+        String projectVersionPattern = "\\d+\\.\\d+\\.\\d+(-SNAPSHOT)?";
+        assertThat(projectVersion, matchesPattern(projectVersionPattern));
     }
 
 }
