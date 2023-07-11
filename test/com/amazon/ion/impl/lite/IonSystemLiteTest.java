fe230c61ec39bc610acf3f90622800ca2d52f2ac
Zack Slayton <zack.slayton@gmail.com>
2023-07-11 17:25:23 -0400

    Makes IonSystemLite load values iteratively, not recursively (#499)
    * Makes IonSystemLite load values iteratively, not recursively
    
    `IonSystemLite` uses recursion to materialize an `IonReader`'s
    current value into an `IonValueLite`. If the data is especially
    deeply nested, this can lead to stack exhaustion.
    
    This patch introduces a private helper class called `ValueLoader`
    that performs the same logic iteratively, avoiding stack exhaustion
    and mitigating the overhead of function call instructions.

---
+++ test/com/amazon/ion/impl/lite/IonSystemLiteTest.java
@@ -0,0 +1,33 @@
+package com.amazon.ion.impl.lite;
+
+import com.amazon.ion.IonSystem;
+import com.amazon.ion.IonValue;
+import com.amazon.ion.system.IonSystemBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IonSystemLiteTest {
+    // Confirms that the standard IonSystem can materialize Ion data that is deeply nested without
+    // exhausting the stack.
+    @Test
+    void materializeDeeplyNested() {
+        final int levelsOfNesting = 1_000_000;
+        final StringBuilder ionText = new StringBuilder(levelsOfNesting * 2);
+        for (int m = 0; m < levelsOfNesting; m++) {
+            ionText.append('[');
+        }
+        for (int m = 0; m < levelsOfNesting; m++) {
+            ionText.append(']');
+        }
+        IonSystem ionSystem = IonSystemBuilder.standard().build();
+        Iterator<IonValue> iterator = ionSystem.iterate(ionText.toString());
+        try {
+            iterator.next();
+        } catch (Exception e) {
+            fail("Encountered an exception: " + e);
+        }
+    }
+}
