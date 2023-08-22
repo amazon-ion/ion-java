23311f2fda7bdb9c0926ed6d150f6169b21cfb2b
Tyler Gregg <greggt@amazon.com>
2023-08-22 12:03:59 -0700

    Clarified that annotation iterators are only valid while positioned on the current value.

--- src/com/amazon/ion/IonReader.java
+++ src/com/amazon/ion/IonReader.java
@@ -225,12 +225,18 @@ public interface IonReader
      * Return the annotations on the curent value as an iterator.  The
      * iterator is empty (hasNext() returns false on the first call) if
      * there are no annotations on the current value.
-     *
+     * <p>
      * Implementations *may* throw {@link UnknownSymbolException} from
      * this method if any annotation contains unknown text. Alternatively,
      * implementations may provide an Iterator that throws
      * {@link UnknownSymbolException} only when the user navigates the
      * iterator to an annotation with unknown text.
+     * <p>
+     * Note: the iterator returned by this method is only valid while
+     * the reader remains positioned on the current value (i.e., before
+     * next, step in, or step out). Use cases that require storing a
+     * value's annotations after advancing past that value should either
+     * copy them from the iterator or call {@link #getTypeAnnotations()}.
      *
      * @return not null.
      */
