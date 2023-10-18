751fcd95f75797167802618a39d570654c8060b0
Matthew Pope <popematt@amazon.com>
2023-10-18 14:07:58 -0700

    Adding support for timestamps

--- src/com/amazon/ion/impl/bin/OpCodes.java
+++ src/com/amazon/ion/impl/bin/OpCodes.java
@@ -20,9 +20,24 @@ public class OpCodes {
     // 0x61-0x6E are additional lengths of decimals.
     public static final byte NEGATIVE_ZERO_DECIMAL = 0x6F;
 
+    public static final byte TIMESTAMP_YEAR_PRECISION = 0x70;
+    public static final byte TIMESTAMP_MONTH_PRECISION = 0x71;
+    public static final byte TIMESTAMP_DAY_PRECISION = 0x72;
+    public static final byte TIMESTAMP_MINUTE_PRECISION = 0x73;
+    public static final byte TIMESTAMP_SECOND_PRECISION = 0x74;
+    public static final byte TIMESTAMP_MILLIS_PRECISION = 0x75;
+    public static final byte TIMESTAMP_MICROS_PRECISION = 0x76;
+    public static final byte TIMESTAMP_NANOS_PRECISION = 0x77;
+    public static final byte TIMESTAMP_MINUTE_PRECISION_WITH_OFFSET = 0x78;
+    public static final byte TIMESTAMP_SECOND_PRECISION_WITH_OFFSET = 0x79;
+    public static final byte TIMESTAMP_MILLIS_PRECISION_WITH_OFFSET = 0x7A;
+    public static final byte TIMESTAMP_MICROS_PRECISION_WITH_OFFSET = 0x7B;
+    public static final byte TIMESTAMP_NANOS_PRECISION_WITH_OFFSET = 0x7C;
+    // 0x7D-0x7F Reserved
 
     public static final byte NULL_UNTYPED = (byte) 0xEA;
     public static final byte NULL_TYPED = (byte) 0xEB;
 
     public static final byte VARIABLE_LENGTH_INTEGER = (byte) 0xF5;
+    public static final byte VARIABLE_LENGTH_TIMESTAMP = (byte) 0xF7;
 }
