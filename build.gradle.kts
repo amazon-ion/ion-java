7f85fd160e5f039c2b7671646393447366824fd7
Matthew Pope <popematt@amazon.com>
2023-11-22 12:14:45 -0800

    Makes SpotBugs ignore Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -290,7 +290,9 @@ tasks {
     project.gradle.startParameter.excludedTaskNames.add(":spotbugsTest")
 
     spotbugsMain {
-        val spotbugsBaselineFile = "$rootDir/config/spotbugs/baseline.xml"
+        val spotbugsConfigDir = "$rootDir/config/spotbugs"
+        excludeFilter.set(file("$spotbugsConfigDir/exclude.xml"))
+        val spotbugsBaselineFile = "$spotbugsConfigDir/baseline.xml"
 
         val baselining = project.hasProperty("baseline") // e.g. `./gradlew :spotbugsMain -Pbaseline`
 
@@ -323,7 +325,7 @@ tasks {
                             "xsltproc",
                             "--output",
                             spotbugsBaselineFile,
-                            "$rootDir/config/spotbugs/baseline.xslt",
+                            "$spotbugsConfigDir/baseline.xslt",
                             "${outputLocation.get()}/spotBugs",
                         )
                     }
