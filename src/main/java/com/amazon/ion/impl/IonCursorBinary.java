2d716bb81ea5aa74365fa94254ee3321ddb22f60
Tyler Gregg <greggt@amazon.com>
2023-12-13 16:48:53 -0800

    Corrects the struct FlexSym transition byte to FlexUInt 0 instead of byte 0.

--- src/main/java/com/amazon/ion/impl/IonCursorBinary.java
+++ src/main/java/com/amazon/ion/impl/IonCursorBinary.java
@@ -10,6 +10,8 @@ import com.amazon.ion.IonCursor;
 import com.amazon.ion.IonType;
 import com.amazon.ion.IvmNotificationConsumer;
 import com.amazon.ion.SystemSymbols;
+import com.amazon.ion.impl.bin.FlexInt;
+import com.amazon.ion.impl.bin.Ion_1_1_Constants;
 import com.amazon.ion.impl.bin.OpCodes;
 
 import java.io.ByteArrayInputStream;
@@ -1206,7 +1208,7 @@ class IonCursorBinary implements IonCursor {
         } else {
             // 0 in field name position of a SID struct indicates that all field names that follow are represented as
             // using FlexSyms.
-            if (buffer[(int) peekIndex] == 0) {
+            if (buffer[(int) peekIndex] == FlexInt.ZERO) {
                 peekIndex++;
                 parent.typeId = IonTypeID.STRUCT_WITH_FLEX_SYMS_ID;
                 fieldSid = (int) uncheckedReadFlexSym_1_1(fieldTextMarker);
@@ -1382,7 +1384,7 @@ class IonCursorBinary implements IonCursor {
         } else {
             // 0 in field name position of a SID struct indicates that all field names that follow are represented as
             // using FlexSyms.
-            if (buffer[(int) peekIndex] == 0) {
+            if (buffer[(int) peekIndex] == FlexInt.ZERO) {
                 peekIndex++;
                 parent.typeId = IonTypeID.STRUCT_WITH_FLEX_SYMS_ID;
                 fieldSid = (int) slowReadFlexSym_1_1(fieldTextMarker);
