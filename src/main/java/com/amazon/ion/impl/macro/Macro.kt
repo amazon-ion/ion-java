c9de7b9d456d1016c7852dc9b0be0520cd5fe486
Matthew Pope <81593196+popematt@users.noreply.github.com>
2024-01-24 16:13:38 -0800

    Adds macro compiler (#697)

--- src/main/java/com/amazon/ion/impl/macro/Macro.kt
+++ src/main/java/com/amazon/ion/impl/macro/Macro.kt
@@ -1,23 +1,45 @@
 package com.amazon.ion.impl.macro
 
-import java.math.BigDecimal
+/**
+ * A [Macro] is either a [SystemMacro] or a [TemplateMacro].
+ */
+sealed interface Macro {
+    val signature: List<Parameter>
+
+    data class Parameter(val variableName: String, val type: ParameterEncoding, val grouped: Boolean)
+
+    enum class ParameterEncoding(val ionTextName: String) {
+        Tagged("any"),
+        // TODO: List all of the possible tagless encodings
+    }
+}
 
 /**
- * Marker interface for Macros
+ * Represents a template macro. A template macro is defined by a signature, and a list of template expressions.
+ * A template macro only gains a name and/or ID when it is added to a macro table.
  */
-sealed interface Macro
+data class TemplateMacro(override val signature: List<Macro.Parameter>, val body: List<TemplateBodyExpression>) : Macro {
+    private val cachedHashCode by lazy { signature.hashCode() * 31 + body.hashCode() }
+    override fun hashCode(): Int = cachedHashCode
 
-/**
- * Represents a template macro. A template macro is defined by a name, a signature, and a list of template expressions.
- */
-data class TemplateMacro(val name: String, val f: BigDecimal, val signature: MacroSignature, val body: List<TemplateExpression>) : Macro
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TemplateMacro) return false
+        // Check the hashCode as a quick check before we dive into the actual data.
+        if (cachedHashCode != other.cachedHashCode) return false
+        if (signature != other.signature) return false
+        if (body != other.body) return false
+        return true
+    }
+}
 
 /**
  * Macros that are built in, rather than being defined by a template.
  */
-enum class SystemMacro : Macro {
-    Stream, // A stream is technically not a macro, but we can implement it as a macro that is the identity function.
-    Annotate,
-    MakeString,
+enum class SystemMacro(override val signature: List<Macro.Parameter>) : Macro {
+    // TODO: replace these placeholders
+    Stream(emptyList()), // A stream is technically not a macro, but we can implement it as a macro that is the identity function.
+    Annotate(emptyList()),
+    MakeString(listOf(Macro.Parameter("text", Macro.ParameterEncoding.Tagged, grouped = true))),
     // TODO: Other system macros
 }
