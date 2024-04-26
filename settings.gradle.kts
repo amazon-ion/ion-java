3eeb7766f306ee7e8346d292f19fd4ecac799c9c
Matthew Pope <81593196+popematt@users.noreply.github.com>
2024-04-26 11:51:08 -0700

    Set Java toolchain for gradle build and fix Sonatype URL (#818)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
+
+// Automatically resolve and download any missing JDK versions
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+}
+
 rootProject.name = "ion-java"
 include("ion-java-cli")
