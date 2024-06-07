34e4e52371a9e0513555514de0b022a150a62f9b
Tyler Gregg <greggt@amazon.com>
2024-06-07 14:12:21 -0700

    Adds the ability to transform Ion 1.0 symbol IDs to Ion 1.1 equivalents when performing a system-level transcoding.

--- src/main/java/com/amazon/ion/impl/_Private_IonWriter.java
+++ src/main/java/com/amazon/ion/impl/_Private_IonWriter.java
@@ -1,22 +1,11 @@
-/*
- * Copyright 2007-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 package com.amazon.ion.impl;
 
 import com.amazon.ion.IonCatalog;
+import com.amazon.ion.IonReader;
 import com.amazon.ion.IonWriter;
+
 import java.io.IOException;
 
 /**
@@ -62,4 +51,52 @@ public interface _Private_IonWriter
 
     /** Indicates whether the writer is stream copy optimized through {@link #writeValue(com.amazon.ion.IonReader)}. */
     public boolean isStreamCopyOptimized();
+
+    @FunctionalInterface
+    interface IntTransformer {
+
+        /**
+         * Transforms an int to another int.
+         * @param original the int to transform.
+         * @return the transformed int.
+         */
+        int transform(int original);
+    }
+
+    /**
+     * Returns the provided int unchanged.
+     */
+    IntTransformer IDENTITY_INT_TRANSFORMER = i -> i;
+
+    /**
+     * Transforms Ion 1.0 local symbol IDs to the equivalent Ion 1.1 local symbol ID. Note: system symbols do not
+     * follow this path.
+     */
+    // TODO change the following once the Ion 1.1 symbol table is finalized. Probably:
+    //   sid10 -> sid10 - SystemSymbols.ION_1_0_MAX_ID;
+    IntTransformer ION_1_0_SID_TO_ION_1_1_SID = IDENTITY_INT_TRANSFORMER;
+
+    /**
+     * Works the same as {@link IonWriter#writeValues(IonReader)}, but transforms all symbol IDs that would otherwise
+     * be written verbatim using the given transform function. This can be used to do a system-level transcode of
+     * Ion 1.0 data to Ion 1.1 while preserving symbol IDs that point to the same text.
+     * @param reader the reader from which to transcode.
+     * @param symbolIdTransformer the symbol ID transform function.
+     * @throws IOException if thrown during write.
+     */
+    default void writeValues(IonReader reader, IntTransformer symbolIdTransformer) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Works the same as {@link IonWriter#writeValue(IonReader)}, but transforms all symbol IDs that would otherwise
+     * be written verbatim using the given transform function. This can be used to do a system-level transcode of
+     * Ion 1.0 data to Ion 1.1 while preserving symbol IDs that point to the same text.
+     * @param reader the reader from which to transcode.
+     * @param symbolIdTransformer the symbol ID transform function.
+     * @throws IOException if thrown during write.
+     */
+    default void writeValue(IonReader reader, IntTransformer symbolIdTransformer) throws IOException {
+        throw new UnsupportedOperationException();
+    }
 }
