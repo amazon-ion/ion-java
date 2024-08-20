f6a9c6161cd47f883d6a6b518c2c5963d920824b
Matthew Pope <81593196+popematt@users.noreply.github.com>
2024-08-20 13:44:15 -0700

    Adds macro evaluator implementation (#924)

---
+++ src/main/java/com/amazon/ion/impl/macro/Environment.kt
@@ -0,0 +1,30 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+package com.amazon.ion.impl.macro
+
+/**
+ * An `Environment` contains variable bindings for a given macro evaluation.
+ *
+ * The [arguments] is a list of expressions for the arguments that were passed to the current macro.
+ * It may also contain other expressions if the current macro invocation is part of a larger evaluation.
+ *
+ * The [argumentIndices] is a mapping from parameter index to the start of the corresponding expression in [arguments].
+ *
+ * The [parentEnvironment] is an environment to use if any of the expressions in this environment
+ * contains a variable that references something from an outer macro invocation.
+ */
+data class Environment private constructor(
+    // Any variables found here have to be looked up in [parentEnvironment]
+    val arguments: List<Expression>,
+    // TODO: Replace with IntArray
+    val argumentIndices: List<Int>,
+    val parentEnvironment: Environment?,
+) {
+    fun createChild(arguments: List<Expression>, argumentIndices: List<Int>) = Environment(arguments, argumentIndices, this)
+    companion object {
+        @JvmStatic
+        val EMPTY = Environment(emptyList(), emptyList(), null)
+        @JvmStatic
+        fun create(arguments: List<Expression>, argumentIndices: List<Int>) = Environment(arguments, argumentIndices, null)
+    }
+}
