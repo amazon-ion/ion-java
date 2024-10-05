36d63da7718439aa8b905856bf67133b647420c8
Matthew Pope <popematt@amazon.com>
2024-10-04 22:29:58 -0700

    Move classes in Macro.kt to their own files

---
+++ src/main/java/com/amazon/ion/impl/macro/TemplateMacro.kt
@@ -0,0 +1,32 @@
+package com.amazon.ion.impl.macro
+
+/**
+ * Represents a template macro. A template macro is defined by a signature, and a list of template expressions.
+ * A template macro only gains a name and/or ID when it is added to a macro table.
+ */
+data class TemplateMacro(override val signature: List<Macro.Parameter>, val body: List<Expression.TemplateBodyExpression>) :
+    Macro {
+    // TODO: Consider rewriting the body of the macro if we discover that there are any macros invoked using only
+    //       constants as arguments—either at compile time or lazily.
+    //       For example, the body of: (macro foo (x)  (values (make_string "foo" "bar") x))
+    //       could be rewritten as: (values "foobar" x)
+
+    private val cachedHashCode by lazy { signature.hashCode() * 31 + body.hashCode() }
+    override fun hashCode(): Int = cachedHashCode
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TemplateMacro) return false
+        // Check the hashCode as a quick check before we dive into the actual data.
+        if (cachedHashCode != other.cachedHashCode) return false
+        if (signature != other.signature) return false
+        if (body != other.body) return false
+        return true
+    }
+
+    override val dependencies: List<Macro> by lazy {
+        body.filterIsInstance<Expression.MacroInvocation>()
+            .map { it.macro }
+            .distinct()
+    }
+}
