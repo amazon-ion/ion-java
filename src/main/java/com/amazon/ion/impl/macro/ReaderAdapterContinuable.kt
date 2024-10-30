ba76b5b95678c50c26a886d651ac72c3c898e15e
Tyler Gregg <greggt@amazon.com>
2024-10-30 13:23:09 -0700

    Ensures that macro arguments are present in the binary reader's buffer before attempting to materialize them.

--- src/main/java/com/amazon/ion/impl/macro/ReaderAdapterContinuable.kt
+++ src/main/java/com/amazon/ion/impl/macro/ReaderAdapterContinuable.kt
@@ -25,15 +25,44 @@ internal class ReaderAdapterContinuable(val reader: IonReaderContinuableCore) : 
         return event != IonCursor.Event.NEEDS_DATA && event != IonCursor.Event.END_CONTAINER
     }
 
-    override fun stringValue(): String = reader.stringValue()
+    /**
+     * Ensures that the value on which the reader is positioned is fully buffered.
+     */
+    private fun prepareValue() {
+        // TODO performance: fill entire expression groups up-front so that the reader will usually not be in slow
+        //  mode when this is called.
+        if (reader.fillValue() != IonCursor.Event.VALUE_READY) {
+            throw IonException("TODO: support continuable reading and oversize value handling via this adapter.")
+        }
+    }
 
-    override fun intValue(): Int = reader.intValue()
+    override fun stringValue(): String {
+        prepareValue()
+        return reader.stringValue()
+    }
 
-    override fun decimalValue(): BigDecimal = reader.decimalValue()
+    override fun intValue(): Int {
+        prepareValue()
+        return reader.intValue()
+    }
 
-    override fun doubleValue(): Double = reader.doubleValue()
+    override fun decimalValue(): BigDecimal {
+        prepareValue()
+        return reader.decimalValue()
+    }
+
+    override fun doubleValue(): Double {
+        prepareValue()
+        return reader.doubleValue()
+    }
 
     override fun stepIntoContainer() {
+        // Note: the following line ensures the entire container is buffered. This improves performance when reading the
+        // container's elements because there is less work to do per element. However, very large containers would
+        // increase memory usage. The current implementation already assumes this risk by eagerly materializing
+        // macro invocation arguments. However, if that is changed, then removing the following line should also be
+        // considered.
+        prepareValue()
         reader.stepIntoContainer()
     }
 
@@ -50,25 +79,49 @@ internal class ReaderAdapterContinuable(val reader: IonReaderContinuableCore) : 
         return annotations
     }
 
-    override fun symbolValue(): SymbolToken = reader.symbolValue()
+    override fun symbolValue(): SymbolToken {
+        prepareValue()
+        return reader.symbolValue()
+    }
 
-    override fun getIntegerSize(): IntegerSize = reader.integerSize
+    override fun getIntegerSize(): IntegerSize {
+        prepareValue()
+        return reader.integerSize
+    }
 
     override fun getFieldNameSymbol(): SymbolToken = reader.fieldNameSymbol
 
     override fun isInStruct(): Boolean = reader.isInStruct
 
-    override fun newBytes(): ByteArray = reader.newBytes()
+    override fun newBytes(): ByteArray {
+        prepareValue()
+        return reader.newBytes()
+    }
 
-    override fun timestampValue(): Timestamp = reader.timestampValue()
+    override fun timestampValue(): Timestamp {
+        prepareValue()
+        return reader.timestampValue()
+    }
 
-    override fun bigIntegerValue(): BigInteger = reader.bigIntegerValue()
+    override fun bigIntegerValue(): BigInteger {
+        prepareValue()
+        return reader.bigIntegerValue()
+    }
 
-    override fun longValue(): Long = reader.longValue()
+    override fun longValue(): Long {
+        prepareValue()
+        return reader.longValue()
+    }
 
     override fun isNullValue(): Boolean = reader.isNullValue
 
-    override fun booleanValue(): Boolean = reader.booleanValue()
+    override fun booleanValue(): Boolean {
+        prepareValue()
+        return reader.booleanValue()
+    }
 
-    override fun integerSize(): IntegerSize? = reader.integerSize
+    override fun integerSize(): IntegerSize? {
+        prepareValue()
+        return reader.integerSize
+    }
 }
