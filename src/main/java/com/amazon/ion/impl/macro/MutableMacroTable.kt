20f197a6cc312c4f74b7eb9faf877cecc81aa3b4
Joshua Barr <jobarr@amazon.com>
2024-11-18 17:10:45 -0800

    Comment asking for more modeling attention in MutableMacroTable

--- src/main/java/com/amazon/ion/impl/macro/MutableMacroTable.kt
+++ src/main/java/com/amazon/ion/impl/macro/MutableMacroTable.kt
@@ -2,6 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.amazon.ion.impl.macro
 
+// This needs modeling attention.
+// - do we want to model an antecedent chain, or have a flat mutable table?
+// - antecedent allows cheap reference to immutable system table or empty table
+// - flat mutable table allows simpler implementation, GC of unneeded values, constant number of lookups
+// - at some point we'll need the capability to communicate immutable encoding contexts to interpret Ion bytes, but
+//   this is neither here nor there
 class MutableMacroTable(private val antecedent: MacroTable) : MacroTable {
     private val macroTable = HashMap<MacroRef, Macro>()
 
