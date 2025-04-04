b08cb98fa7542c93e258b3205969db0e1fe78685
Joshua Barr <70981087+jobarr-amzn@users.noreply.github.com>
2025-04-04 11:29:44 -0700

    Upgrade plugins and JDKs for build stability (#1061)
    * Upgrade JMH and Spotless plugins
    
    Additionally, disable :spotbugsJmh. These combined changes are necessary
    to get our release build to work while building with modern JDKs.
    
    * Remove JDK 8 builds, still targeting Java 8
    
    * Specify Java 11 for CodeQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
     id("com.github.spotbugs") version "5.0.13"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
 
-    id("com.diffplug.spotless") version "6.11.0"
+    id("com.diffplug.spotless") version "7.0.2"
 
     // Used for generating the third party attribution document
     id("com.github.jk1.dependency-license-report") version "2.5"
@@ -46,7 +46,7 @@ plugins {
     // the "spotbugsMain" task, causing it to fail. Instead, we will create a separate task to generate the bundle info.
     id("biz.aQute.bnd.builder") version "6.4.0" apply false
 
-    id("me.champeau.jmh") version "0.7.2"
+    id("me.champeau.jmh") version "0.7.3"
 }
 
 jacoco {
@@ -217,10 +217,17 @@ lateinit var sourcesJar: AbstractArchiveTask
 lateinit var javadocJar: AbstractArchiveTask
 lateinit var minifyJar: ProGuardTask
 
+tasks.withType<JavaCompile>().configureEach {
+    javaCompiler = javaToolchains.compilerFor {
+        languageVersion = JavaLanguageVersion.of(8)
+    }
+}
+
 tasks {
     withType<JavaCompile> {
         options.encoding = "UTF-8"
     }
+
     withType<KotlinCompile<KotlinJvmOptions>> {
         kotlinOptions {
             // Kotlin jvmTarget must match the JavaCompile release version
@@ -400,6 +407,9 @@ tasks {
     // spotbugs-gradle-plugin creates a :spotbugsTest task by default, but we don't want it
     // see: https://github.com/spotbugs/spotbugs-gradle-plugin/issues/391
     project.gradle.startParameter.excludedTaskNames.add(":spotbugsTest")
+    // same for :spotbugsJmh, we don't need to run Spotbugs on our JMH code
+    // Alternatively we *could* set the toolchain for this task as we do for :spotBugsMain, but this is easier.
+    project.gradle.startParameter.excludedTaskNames.add(":spotbugsJmh")
 
     spotbugsMain {
         launcher.set(
