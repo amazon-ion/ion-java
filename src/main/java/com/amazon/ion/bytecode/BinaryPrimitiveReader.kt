602029855c7984d1b7e6750dfb54a085b286aa64
austnwil <austnwil@amazon.com>
2025-10-14 15:14:46 -0700

    Fix failing tests related to readFixedInt24

--- src/main/java/com/amazon/ion/bytecode/BinaryPrimitiveReader.kt
+++ src/main/java/com/amazon/ion/bytecode/BinaryPrimitiveReader.kt
@@ -58,7 +58,9 @@ object BinaryPrimitiveReader {
     private fun ByteArray.getInt24(position: Int): Int {
         return (this[position].toInt() and 0xFF) or
             ((this[position + 1].toInt() and 0xFF) shl 8) or
-            ((this[position + 2].toInt() and 0xFF) shl 16)
+            // Shift left into 4th byte and then back down a byte here spreads the sign
+            // across high byte, which is needed for negatives
+            ((this[position + 2].toInt() and 0xFF) shl 24 shr 8)
     }
     @JvmStatic
     private fun ByteArray.getInt(position: Int): Int {
