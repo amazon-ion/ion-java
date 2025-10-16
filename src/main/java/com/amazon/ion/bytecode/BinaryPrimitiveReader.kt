f21abb2b4e9dc2ad30154ce8d1607bf5943e4458
austnwil <austnwil@amazon.com>
2025-10-16 10:08:22 -0700

    Add TODOs for if-else vs catch-throw tests

--- src/main/java/com/amazon/ion/bytecode/BinaryPrimitiveReader.kt
+++ src/main/java/com/amazon/ion/bytecode/BinaryPrimitiveReader.kt
@@ -83,36 +83,42 @@ object BinaryPrimitiveReader {
 
     @JvmStatic
     fun ByteArray.readFixedInt8AsShort(start: Int): Short {
+        // TODO: ion-java#1114
         if (this.size < start + 1) throw IonException("Incomplete data: start=$start, length=${1}, limit=${this.size}")
         return this[start].toShort()
     }
 
     @JvmStatic
     fun ByteArray.readFixedInt16AsShort(start: Int): Short {
+        // TODO: ion-java#1114
         if (this.size < start + 2) throw IonException("Incomplete data: start=$start, length=${2}, limit=${this.size}")
         return this.getShort(start)
     }
 
     @JvmStatic
     fun ByteArray.readFixedInt24AsInt(start: Int): Int {
+        // TODO: ion-java#1114
         if (this.size < start + 3) throw IonException("Incomplete data: start=$start, length=${3}, limit=${this.size}")
         return this.getInt24(start)
     }
 
     @JvmStatic
     fun ByteArray.readFixedInt32AsInt(start: Int): Int {
+        // TODO: ion-java#1114
         if (this.size < start + 4) throw IonException("Incomplete data: start=$start, length=${4}, limit=${this.size}")
         return this.getInt(start)
     }
 
     @JvmStatic
     fun ByteArray.readFixedIntAsInt(start: Int, length: Int): Int {
+        // TODO: ion-java#1114
         if (this.size < start + length) throw IonException("Incomplete data: start=$start, length=$length, limit=${this.size}")
         return (this.getInt(start - 4 + length) shr ((4 - length) * 8))
     }
 
     @JvmStatic
     fun ByteArray.readFixedIntAsLong(start: Int, length: Int): Long {
+        // TODO: ion-java#1114
         if (this.size < start + length) throw IonException("Incomplete data: start=$start, length=$length, limit=${this.size}")
         if (length > 4) {
             // TODO: See if we can simplify some of the calculations
