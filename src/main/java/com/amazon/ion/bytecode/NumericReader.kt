96d1bb79e49ef66d9a98e9d4d24197e2f2fcf44a
austnwil <austnwil@amazon.com>
2025-10-16 21:21:03 -0700

    Simplify FP16 to FP32 conversion algorithm

--- src/main/java/com/amazon/ion/bytecode/NumericReader.kt
+++ src/main/java/com/amazon/ion/bytecode/NumericReader.kt
@@ -11,12 +11,16 @@ import com.amazon.ion.IonException
 internal object NumericReader {
     private const val NAN_EXPONENT_FP16 = 0b01111100_00000000
     private const val NAN_EXPONENT_FP32 = 0b01111111_10000000_00000000_00000000
-    private const val EXPONENT_BIAS_FP16 = 15
-    private const val EXPONENT_BIAS_FP32 = 127
+    private const val EBIAS_FP16 = 15
+    private const val EBIAS_FP32 = 127
+    private const val F16_EBIAS_CORRECTION = EBIAS_FP32 - EBIAS_FP16
     private const val MANTISSA_SIZE_FP16 = 10
     private const val MANTISSA_SIZE_FP32 = 23
-    private const val MANTISSA_MASK_FP32 = 0b01111111_11111111_11111111
-    private const val BIT_LEFT_OF_MANTISSA_MASK_FP32 = 0b10000000_00000000_00000000
+    private const val F16_MANTISSA_CORRECTION = MANTISSA_SIZE_FP32 - MANTISSA_SIZE_FP16
+    private const val MANT_MASK_FP32 = 0b01111111_11111111_11111111
+    private const val SIGN_MASK_FP16 = 0b10000000_00000000
+    private const val EXPONENT_MASK_FP16 = 0b01111100_00000000
+    private const val MANTISSA_MASK_FP16 = 0b00000011_11111111
 
     /**
      * Returns the 2-byte int at the given position in the array. Does NOT validate array bounds.
@@ -78,22 +82,17 @@ internal object NumericReader {
         if (this.size < start + 2) throw IonException("Incomplete data: start=$start, length=${2}, limit=${this.size}")
 
         val bits16 = this.getShort(start).toInt()
-        val sign16 = bits16 and 0b10000000_00000000
-        val exponent16 = bits16 and 0b01111100_00000000
-        val mantissa16 = bits16 and 0b00000011_11111111
+        val sign16 = bits16 and SIGN_MASK_FP16
+        val exponent16 = bits16 and EXPONENT_MASK_FP16
+        val mantissa16 = bits16 and MANTISSA_MASK_FP16
 
         // Move sign into position
         val sign32 = sign16 shl 16
         // Exponent needs determined based on branching unfortunately
         var exponent32: Int
-        // Move 10 mantissa bits to upper portion of the new 23 mantissa bits
-        // TODO: we don't need to do this specific operation for 0e0 or -0e0, and we already do the branching to detect
-        //  these cases anyway. We can move assignment to mantissa32 into the when branches and set to zero in these
-        //  cases.
-        var mantissa32 = mantissa16 shl (MANTISSA_SIZE_FP32 - MANTISSA_SIZE_FP16)
+        var mantissa32: Int
 
-        // TODO: these operations can probably be simplified. Avoiding the branching/looping would be nice, but I could
-        //  not find a way to normalize subnormal values without it.
+        // TODO: these operations can probably be simplified further.
         when (exponent16) {
             0 -> {
                 if (mantissa16 != 0) {
@@ -101,17 +100,13 @@ internal object NumericReader {
                      * The float16 is subnormal; it needs normalization. To normalize, we need to shift the mantissa
                      * left until its first non-zero bit leaves the mantissa field. This is because normalized
                      * floats have an implied leading digit of 1 before the decimal point, whereas non-normalized ones
-                     * have an implied leading digit of 0. We also have to decrement the exponent while we do this.
+                     * have an implied leading digit of 0. We also have to decrement the exponent.
                      */
-                    exponent32 = (1 + EXPONENT_BIAS_FP32 - EXPONENT_BIAS_FP16)
-                    while (mantissa32 and BIT_LEFT_OF_MANTISSA_MASK_FP32 == 0) {
-                        mantissa32 = mantissa32 shl 1
-                        exponent32--
-                    }
-                    // Remove the extra bit to the left of the mantissa
-                    mantissa32 = (mantissa32 and MANTISSA_MASK_FP32)
-                    // Shift the exponent into place
-                    exponent32 = exponent32 shl MANTISSA_SIZE_FP32
+                    val leadingZeros = Integer.numberOfLeadingZeros(mantissa16.shl(Int.SIZE_BITS - MANTISSA_SIZE_FP16))
+                    val shiftSize = leadingZeros + 1
+                    mantissa32 = mantissa16.shl(F16_MANTISSA_CORRECTION + shiftSize)
+                        .and(MANT_MASK_FP32) // Remove the extra bit to the left of the mantissa
+                    exponent32 = (1 + F16_EBIAS_CORRECTION - shiftSize).shl(MANTISSA_SIZE_FP32)
                 } else {
                     // exponent zero and mantissa zero = 0e0 or -0e0. Nothing more needed.
                     exponent32 = 0
@@ -120,19 +115,22 @@ internal object NumericReader {
             }
             NAN_EXPONENT_FP16 -> {
                 // Value is NaN or Inf. New exponent has to have all bits set.
-                // The mantissa is already set correctly. The 10 bytes from the f16 are at the high end of the mantissa
-                // for the f32, so the MSB with NaN signaling semantics is the same. There will be 13 zero bits at the
-                // low end of the new mantissa.
                 exponent32 = NAN_EXPONENT_FP32
+                // Move the 10 mantissa bits to upper portion of the new 23 mantissa bits. The 10 bytes from the f16 are
+                // now at the high end of the mantissa for the f32, so the MSB with NaN signaling semantics is the same.
+                // There will be 13 zero bits at the low end of the new mantissa.
+                mantissa32 = mantissa16.shl(F16_MANTISSA_CORRECTION)
             }
             else -> {
                 // This is a normal number. We can just adjust the exponent to use the correct bias and be done.
-                exponent32 = (((exponent16 shr MANTISSA_SIZE_FP16) - EXPONENT_BIAS_FP16 + EXPONENT_BIAS_FP32) and 0xFF) shl MANTISSA_SIZE_FP32
+                exponent32 = (exponent16.shr(MANTISSA_SIZE_FP16) + F16_EBIAS_CORRECTION)
+                    .shl(MANTISSA_SIZE_FP32)
+                // Move 10 mantissa bits to upper portion of the new 23 mantissa bits.
+                mantissa32 = mantissa16.shl(F16_MANTISSA_CORRECTION)
             }
         }
 
         val bits32 = sign32 or exponent32 or mantissa32
-
         return Float.fromBits(bits32)
     }
 
